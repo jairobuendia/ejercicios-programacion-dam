b9ed51ad75cd7ac5b1c1d0156fdebff10e5fd8b2
Jairo Buendia <jairobuendialopez@gmail.com>
2020-10-20 10:17:00 +0200

    Nueva actualizacion

---
+++ JAVA/Tema2/ejercicio1.java
@@ -0,0 +1,16 @@
+/**
+ * Muestra por pantalla distintas operaciones con x e y!
+ *
+ * @author Jairo Buendia
+ */
+public class ejercicio1 { // Clase principal
+  public static void main(String[] args) {
+    int x = 144;
+    int y = 199;
+    System.out.println("Suma de x + y = " + (x + y));
+    System.out.println("Resta de x - y = " + (x - y));
+    System.out.println("Multiplicación de x * y = " + (x * y));
+    System.out.println("División de x / y = " + (x / y));
+
+  }
+}
---
+++ JAVA/Tema2/ejercicio2.java
@@ -0,0 +1,12 @@
+/**
+ * Asigna tu nombre a una variable y muestralo por pantalla!
+ *
+ * @author Jairo Buendia
+ */
+public class ejercicio2 { // Clase principal
+  public static void main(String[] args) {
+    String nombre = "Jairo Buendía";
+    System.out.println("Mi nombre completo es: " + nombre);
+
+  }
+}
