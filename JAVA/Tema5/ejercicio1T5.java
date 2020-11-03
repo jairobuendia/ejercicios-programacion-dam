ac5457a4d236a15d273d1a60f67d96a19d8fd963
Jairo Buendia <jairobuendialopez@gmail.com>
2020-11-03 10:00:42 +0100

    update

---
+++ JAVA/Tema5/ejercicio1T5.java
@@ -0,0 +1,12 @@
+ /**
+ * Muestra los números multiplos de 5 de 0 a 100 utilizando un bucle.
+ * @author Jairo Buendia
+ */
+public class ejercicio1T5 { 
+  public static void main(String[] args) {
+    
+    for (int num = 5; num < 105 ; num = num + 5 ) {
+      System.out.println(num);
+    }
+  }
+}
---
+++ JAVA/Tema5/ejercicio4T5.java
@@ -0,0 +1,12 @@
+/**
+ * Muestra los números del 320 al 160, contando de 20 en 20 haciia atras utilizando un bucle.
+ * @author Jairo Buendia
+ */
+public class ejercicio4T5 { 
+  public static void main(String[] args) {
+    
+    for (int num = 320; num > 140 ; num = num - 20 ) {
+      System.out.println(num);
+    }
+  }
+}
